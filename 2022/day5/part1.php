728b91ba68f329bec9eddad2563e6cefb6d65228
Aljaz <florjancicaljaz@gmail.com>
2022-12-05 18:09:24 +0100

    day 5 part 1

---
+++ 2022/day5/part1.php
@@ -0,0 +1,32 @@
+<?php
+// $overlaps = 0;
+$fullString = file_get_contents('./input.txt');
+[$stacksString, $commandsString] = preg_split("/\n\n/", $fullString);
+$commands = preg_split("/\r\n|\n|\r/", $commandsString);
+$stacks = [
+  ['G', 'D', 'V', 'Z', 'J', 'S', 'B'],
+  ['Z', 'S', 'M', 'G', 'V', 'P'],
+  ['C', 'L', 'B', 'S', 'W', 'T', 'Q', 'F'],
+  ['H', 'J', 'G', 'W', 'M', 'R', 'V', 'Q'],
+  ['C', 'L', 'S', 'N', 'F', 'M', 'D'],
+  ['R', 'G', 'C', 'D'],
+  ['H', 'G', 'T', 'R', 'J', 'D', 'S', 'Q'],
+  ['P', 'F', 'V'],
+  ['D', 'R', 'S', 'T', 'J'],
+];
+
+foreach ($commands as $command) {
+  $numbers = preg_match_all('!\d+!', $command, $matches);
+  $actualCommands = $matches[0];
+  for ($x = 0; $x < $actualCommands[0]; $x++) {
+    array_push(
+      $stacks[$actualCommands[2] - 1],
+      array_pop($stacks[$actualCommands[1] - 1])
+    );
+  }
+}
+
+foreach ($stacks as $stack) {
+  echo array_pop($stack);
+}
+?>
